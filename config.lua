b48e261d9014b569160c265f9e45323d0d085328
Kortoei tongmanee <kortoei@Kortoeis-MacBook-Air.local>
2022-07-29 12:14:52 +0700

    change terminal key

--- config.lua
+++ config.lua
@@ -53,15 +53,18 @@ lvim.builtin.which_key.mappings["y"] = {
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
+
+-- Toggleterm
+lvim.builtin.terminal.open_mapping = [[<c-\>]]
 lvim.builtin.which_key.mappings["t"] = {
   name = "Toggleterm",
-  n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-  u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-  t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-  p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-  f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+  t = { "<cmd>ToggleTerm direction=float<cr>", "Float Terminal" },
   h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
   v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+  -- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+  -- u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
+  -- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
+  -- t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
 }
 
 -- TODO: User Config for predefined plugins
